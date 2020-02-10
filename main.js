fa06cedad5e8cbee9e1f39cac1e73d90bc4b91c4
Алексей Жуковский <alexey@poravinternet.ru>
2020-02-10 09:34:15 +0300

    Стилизация, кнопка закладки

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
-let position=localStorage.getItem('position');
-if (position!=undefined){
-
-    window.scrollBy({
-        top: position,
-        behavior: 'smooth'
-    });
+// Закладка, хранится в кеше
+let position = localStorage.getItem('position');
+if (position == undefined) {
+    bookmark();
 }
 
-window.addEventListener('scroll', function() {
-    console.log(window.pageYOffset)
-    position=window.pageYOffset;
-    localStorage.setItem('position', position);
-    console.log(localStorage.getItem('position'))
-});
+function bookmark() {
+    if (position != undefined) {
+
+        window.scrollBy({
+            top: localStorage.getItem('position') - window.pageYOffset,
+            behavior: 'smooth'
+        });
+    }
+    window.addEventListener('scroll', function () {
+
+        position = window.pageYOffset;
+        localStorage.setItem('position', position);
+
+    })
+    document.getElementById('bookmark').classList.add("bookmark-hide");
+}
+
+//опен меню
+let closexxx = function (event) {
+    let e = document.getElementById('menu');
+    if (!e.contains(event.target)) {
+        document.getElementById('menu-trigger').classList.toggle("close-btn");
+        document.getElementById("menu").classList.toggle("is-active");
+        document.removeEventListener('click', closexxx, false);
+    }
+};
+
+function menuTrigger() {
+    let trigger = document.getElementById('menu-trigger'), menu = document.getElementById("menu");
+
+    menu.classList.toggle("is-active");
+    trigger.classList.toggle("close-btn");
+    function vvv() {
+        setTimeout(function(){
+            if (document.getElementById('menu-trigger').classList.contains('close-btn')) {
+                document.addEventListener('click',  closexxx, false);
+            }}, 3);
+    }
+   //vvv();
+
+}
